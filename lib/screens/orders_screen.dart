5be340e0bf4528925e410f5b04eb5dc33852279f
Jignesh Pandav <pandav.jignesh.n@gmail.com>
2022-03-24 16:19:44 +0530

    implemented futurebuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,56 +5,38 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart' as _oi;
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = "/orders";
 
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      setState(() {
-        _isLoading = true;
-      });
-      Provider.of<Orders>(context, listen: false).fetchOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Orders"),
-      ),
-      drawer: const AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return _oi.OrderItem(order: orderData.orders[index]);
-              },
-              itemCount: orderData.orders.length,
-            ),
-    );
+        appBar: AppBar(
+          title: const Text("Orders"),
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) {
+                  return ListView.builder(
+                    itemBuilder: (ctx, index) {
+                      return _oi.OrderItem(order: orderData.orders[index]);
+                    },
+                    itemCount: orderData.orders.length,
+                  );
+                },
+              );
+            }
+          },
+        ));
   }
 }
