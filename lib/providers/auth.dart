d2c0f05ec771bcb47681040ccfdada5be2219dce
jigneshpandav <pandav.jignesh.n@gmail.com>
2022-04-02 14:38:37 +0530

    Worked on user auto login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,14 +1,17 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/configs/environment.dart';
-import 'package:flutter_shop_app/models/http_exceptions.dart';
+import '../configs/environment.dart';
+import '../models/http_exceptions.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth with ChangeNotifier {
-  late String _token;
+  String? _token;
   DateTime? _expiryDate;
-  late String _userId;
+  String? _userId;
+  Timer? _authTimer;
 
   bool get isAuthenticated {
     return token != null;
@@ -21,7 +24,7 @@ class Auth with ChangeNotifier {
     return null;
   }
 
-  String get userId {
+  String? get userId {
     return _userId;
   }
 
@@ -32,10 +35,12 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> authenticate(
-      String email, String password, String identifier) async {
+    String email,
+    String password,
+    String identifier,
+  ) async {
     try {
       final url = _url(identifier);
-      print('url: $url');
       final response = await http.post(
         url,
         body: json.encode({
@@ -45,7 +50,6 @@ class Auth with ChangeNotifier {
         }),
       );
       final responseData = json.decode(response.body);
-      print(responseData);
       if (responseData['error'] != null) {
         throw HttpException(responseData['error']['message']);
       }
@@ -56,9 +60,56 @@ class Auth with ChangeNotifier {
           seconds: 3600,
         ),
       );
+      _autoLogout();
       notifyListeners();
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode({
+        'token': _token!,
+        'userId': _userId!,
+        'expiryDate': _expiryDate!.toIso8601String()
+      });
+      prefs.setString('userData', userData);
     } catch (error) {
       rethrow;
     }
+  }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+      _authTimer = null;
+    }
+    notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+    }
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
+  }
+
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final userData = json.decode(
+      prefs.getString("userData") as String,
+    ) as Map<String, dynamic>;
+    final expiryDate = DateTime.parse(userData['expiryDate'] as String);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token = userData['token'] as String;
+    _userId = userData['userId'] as String;
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
   }
 }
